0f162dfceb9d80867cd578429b6e45a1d7f1974c
Marijana Gligoric <Marijana.Gligoric@rt-rk.com>
2019-01-15 16:18:54 +0100

    Passing parameteres

--- C.c
+++ C.c
@@ -1,3 +1,19 @@
 scanf("\n");
 scanf("%[^\n]%*c", &s)        -->citanje cele linije iz konzole
+%d, %s, %c, %f, %lf
 ****************************************************************************
+poraditi na onim ogranicenjima u sabiranju brojeva
+kad moze doci do owerflova kod pormenjive
+****************************************************************************
+function(int param)
+int variable
+function(variable) ---------> prosledjivanje po vrednosti
+
+function(int *param)
+int variable
+function(&variable)  --------> prosledjivanje po referenci
+
+moze takodje i da kazes
+int p* = &varibale
+function(p)
+***************************************************************************
